b32f794610e0ac022f17f357811e43aee1c1980e
kaobarry <codpropm@gmail.com>
2023-05-26 12:23:32 +0800

    first version with slide show

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <SlideShow/>
+</template>
+
+<script>
+import SlideShow from './components/SlideShow.vue'
+export default {
+  name: 'App',
+  components: {
+    SlideShow
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SlideShow.vue
@@ -0,0 +1,33 @@
+<template>
+<vueper-slides bullets-outside :dragging-distance="50"  :autoplay=true :slideRatio="1/6">
+  <vueper-slide
+    v-for="(slide, i) in slides"
+    :key="i"
+    :image="slide.image"
+    :title="slide.title"
+    :content="slide.content" />
+</vueper-slides>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+export default {
+  name: 'SlideShow',
+  components: { VueperSlides, VueperSlide },
+  data() {
+    return {
+      slides: [
+                {
+                    image: require('@/assets/03.jpg'),
+                },
+                {
+                    image: require('@/assets/04.jpg'),
+   
+                }
+            ]
+    }
+  },
+}
+</script>
+
