edb9b8f2d860d1e2dd6914b5ba7a5ab8aa26c5b5
yukina <codpropm@gmail.com>
2023-06-21 15:00:09 +0800

    fix css of choice quiz buttons

--- src/views/AnswerGroupView.vue
+++ src/views/AnswerGroupView.vue
@@ -4,21 +4,21 @@
       <div class="fixed-bottom">
         <div class="navbar-light" role="group">
           <div class="btn-group bottom-btns" style="height:70%">
-            <b-button class="btn btn-outline-light" variant="outline-light"  @click="previousBtn">
+            <b-button class="btn btn-light"  @click="previousBtn">
             &nbsp;&nbsp;
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
               </svg>
               上一題
             </b-button>
-            <b-button id="liveToastBtn" variant="outline-light" @click="listBtn">
+            <b-button class="btn btn-light" @click="listBtn">
             &nbsp;
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-nested" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z" />
               </svg>
               答題進度
             </b-button>
-            <b-button id="onboarding-2" class="btn btn-light"  @click="nextBtn">
+            <b-button class="btn btn-light"  @click="nextBtn">
             &nbsp;&nbsp;
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
@@ -139,5 +139,15 @@
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped lang="scss">
-    
+  .navbar-light{
+    background-color: #4186ED;
+    .bottom-btns{
+      margin-top: 10px;
+      margin-bottom: 10px;
+      width: 95%;
+      label{
+        margin: 0;
+      }
+    }
+  }
   </style>
