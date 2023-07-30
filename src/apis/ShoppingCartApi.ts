74c7d74bbeee87d189c53635435e1658753f527d
WowShiba <oopsdxm@gmail.com>
2023-07-30 10:31:47 +0800

    Update

--- src/apis/ShoppingCartApi.ts
+++ src/apis/ShoppingCartApi.ts
@@ -3,9 +3,9 @@ import vm from '@/main';
 
 export default class CategoryApi {
 
-    static async addProductToShoppingCart(sellPlanId: string,contents: string): Promise<string> {
-        const result = await api.post(`/shoppingcar/addContent/${vm.$cookies.get("")}`, {categoryId: categoryId});
+    static async addProductToShoppingCart(sellPlanId: string, contents: []): Promise<string> {
+        const shoppingCartId = vm.$cookies.get("user").shoppingCar.uuid;
+        const result = await api.post(`/shoppingcar/addContent/${shoppingCartId}`, {uuid: sellPlanId, contents: contents});
         return result.data;
     }
-
 }
