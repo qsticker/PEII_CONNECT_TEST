3a20e24069b0a711aaefef548f8aab27bd40eee2
SkylerTsai <45330963+SkylerTsai@users.noreply.github.com>
2023-09-20 16:05:49 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: 'dist',
-  publicPath: '/PEII_CONNECT_TEST/',
+  publicPath: '/',
   transpileDependencies: true,
   lintOnSave : false
 })
